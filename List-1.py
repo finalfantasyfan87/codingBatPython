c05a7926e3c9ed44093484008dd70937640b46a5
Jenster <finalfantasyguru8705@gmail.com>
2019-09-24 01:43:05 -0500

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -1,1 +1,15 @@
 
+def first_last6(nums):
+  if nums[0] == 6 or nums[len(nums)-1] == 6:
+    return True
+  else:
+    return False
+  
+  def same_first_last(nums):
+  if len(nums) >=1 and nums[0] == nums[len(nums)-1]:
+    return True
+  else:
+    return False
+
+  def make_pi():
+  return [3,1,4]
