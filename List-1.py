ba5005a655ee6214fe246da622bf3b93588cf9c0
Jenster <finalfantasyguru8705@gmail.com>
2019-09-24 01:44:15 -0500

    Update List-1.py

--- List-1.py
+++ List-1.py
@@ -13,3 +13,20 @@ def first_last6(nums):
 
   def make_pi():
   return [3,1,4]
+
+def common_end(a, b):
+  if a[0] == b[0] or a[len(a)-1] == b[len(b)-1]:
+    return True
+  else:
+    return False
+
+def sum3(nums):
+  return nums[0]+nums[1]+nums[2]
+
+def rotate_left3(nums):
+  newNums = [nums[1],nums[2], nums[0]]
+  return newNums
+
+def reverse3(nums):
+  return nums[::-1]
+
